743f1e638c5bd3b3bf790d53fba1391e970fb7eb
Amir <ar.anjomshoaa@gmail.com>
2019-12-10 16:18:11 -0800

    Join Function works

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ const App = () => {
           <Route path="/gameslobby/:id" component={GameTable} />
           <Route path="/chat" component={chat} />
         </Switch>
+        
       </div>
     </Router>
   )
